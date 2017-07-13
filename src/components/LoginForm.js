552be3d920b68141e75a824581d68c8ef18da2ee
Zach Lipps <zacharylipps10@gmail.com>
2017-07-12 17:44:52 -0700

    add loginform

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-import { Card } from './common';
+import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
   render() {
     return (
-      <Card />
+      <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            placeholder="email@gmail.com"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="password"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button
+            Login
+          />
+        </CardSection>
+      </Card>
     );
   }
 }
